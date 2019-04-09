6fbbaede424fcc0012d5335056550aa7159252a6
yeongsunpark <ylunar@naver.com>
2019-04-09 15:38:49 +0900

    마커가 있는본문에서 답의 위치를 찾는 것

---
+++ nia/find_location.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created by YeongsunPark at 2019-03-25
+
+import re
+def find_index(a_para, answer):
+    try:
+        a_location = a_para.index("{}{}{}".format("|" * 5, answer, "|" * 5))
+        markers = list()
+        for m in ["|"]:
+            markers += [match.start() for match in re.finditer(re.escape(m * 5), a_para)]
+        a_location -= len([x for x in markers if x < a_location]) * 5
+        a_para_strip = a_para
+        for m in ["|"]:
+            a_para_strip = a_para_strip.replace(m * 5, "")
+        new_answer = a_para_strip[a_location:a_location + len(answer)]
+
+        try:
+            assert (answer == new_answer)
+        except AssertionError:
+            print ("assertionError")
+            exit()
+    except ValueError as e:
+        print (a_para+"\n"+answer)
+        exit()
+    return a_location
