a71c647c7bf8764a9e09d0b3d375dec7569c8d2c
yeongsunpark <ylunar@naver.com>
2019-04-09 15:18:28 +0900

    count_answer2_old (count_answer.py 를 한 폴더에 대해 실행)

--- nia/count_answer.py
+++ nia/count_answer.py
@@ -4,15 +4,21 @@
 # Created by YeongsunPark at 2019-03-06
 
 import os
+import logging
 
 
-f2 = open("/home/msl/ys/cute/nia/no_marker/error_checker.tsv","w")
-f3 = open("/home/msl/ys/cute/nia/no_marker/normal_checker.tsv","w")
+# f2 = open("/home/msl/ys/cute/nia/no_marker/error_checker.tsv","w")
+# f3 = open("/home/msl/ys/cute/nia/no_marker/normal_checker.tsv","w")
 
 for f in os.listdir("/home/msl/ys/cute/nia/no_marker"):
     if "no_checker" not in f:
         continue
+    if "songjuhyeong" not in f:
+        continue
     creator = f.split("_")[2].split(".")[0]
+    logging.error(creator)
+    f2_list = []
+    f3_list = []
 
     # f = open("no_checker.tsv", "r")
     with open(os.path.join("/home/msl/ys/cute/nia/no_marker", f), "r") as f:
@@ -43,23 +49,35 @@ for f in os.listdir("/home/msl/ys/cute/nia/no_marker"):
 
             a = content.count(answer)  # 컨텐츠에서 answer 가 몇 개 있는지 찾을 건데
             if a != 1:  # 일이 아니면 ㅋㅋㅋ 오류야!!!
-                f2.write("\t".join([t, str(num), item[2], item[3], item[4], l, str(a)]))
-                f2.write("\n")
+                f2_list.append([t, str(num), item[2], item[3], item[4], l, str(a)])
+                # f2.write("\t".join([t, str(num), item[2], item[3], item[4], l, str(a)]))
+                # f2.write("\n")
+                with open(os.path.join("/home/msl/ys/cute/nia/no_marker/", "error_{}.tsv".format(creator)),
+                          'w', encoding='utf8') as f2:
+                    for fl in f2_list:
+                        for ff in fl:
+                            f2.write(str(ff) + "\t")
+                        f2.write("\n")
+
 
             elif a == 1:
                 start = (l.find(answer))
                 m_context = l[:start] + "|" * 5 + answer + "|" * 5 + l[start + len(answer):]
-                # f3.write("\t".join([t, l, item[2], item[3], item[4], str(a), str(start), m_context]))
-                # title, 본문원본, 육하원칙, 질문, 답변, 본문 안 답변의 개수, 답변의 시작 위치, 마커가 있는 본문
-                f3.write("\t".join(
-                    [t, m_context, str(num), item[2], item[3], item[4], l, str(start), str(a), str(a + len(answer))]))
-                # TITLE	CONTENT	질문번호	질문	답변	기사일자(대신 정답의 수)	원본	답시작위치	답끝위치
-                f3.write("\n")
+                # f3_list.append([t, m_context, str(num), item[2], item[3], item[4], l, str(start), str(a), str(a + len(answer))])
+                f3_list.append([t, m_context, str(num), item[3], item[4], "", "", l, str(start), str(start + len(answer))])  # common_tsv 를 위해 수정
+                # f3.write("\t".join([t, m_context, str(num), item[2], item[3], item[4], l, str(start), str(a), str(a + len(answer))]))
+                # f3.write("\n")
+                with open(os.path.join("/home/msl/ys/cute/nia/no_marker/", "normal_{}.tsv".format(creator)),
+                          'w', encoding='utf8') as f3:
+                    for fl in f3_list:
+                        f3.write("\t".join(fl))
+                        f3.write("\n")
             else:
                 print(line)
                 break
             num += 1
 
+
     f.close()
-f2.close()
-f3.close()
+# f2.close()
+# f3.close()
