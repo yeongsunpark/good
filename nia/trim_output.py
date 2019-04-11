eb0a05fdc71afda1e23819a466befe5090193353
yeongsunpark <ylunar@naver.com>
2019-04-11 11:28:16 +0900

    trim_output을 sys.argv 이용하는 것으로 수정

--- nia/trim_output.py
+++ nia/trim_output.py
@@ -4,9 +4,14 @@
 # Created by YeongsunPark at 2019-03-12
 
 import re
+import sys
+
+sys.path.insert(0, '')
+
+# 사용방법: python3 trim_output.py /home/msl/ys/cute/nia/output.tsv /home/msl/ys/cute/nia/db_news/output2.tsv
 
 result = []
-with open ("output.txt") as f:
+with open (sys.argv[1]) as f:
     i = 0
     for line in f:
         item = line.split("\t")
@@ -31,7 +36,7 @@ with open ("output.txt") as f:
 
         # result.append("\t".join([item[0], context]))
 
-f2 = open("nocut_jan2mar.txt", "w")
+f2 = open(sys.argv[2], "w")
 for r in result:
     f2.write(r)
     # f2.write("\n")
