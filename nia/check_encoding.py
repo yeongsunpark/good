dfbc8dd6d5094c3781c9f90393e9a08307175e6e
yeongsunpark <ylunar@naver.com>
2019-04-29 17:56:54 +0900

    cp949로 되어 있는 문자를 utf-8로 보기

--- nia/check_encoding.py
+++ nia/check_encoding.py
@@ -15,10 +15,7 @@ print (c)
 d = c.decode('cp949')
 print (d)
 """
-"""
+
 a = "\uc54c\ub809\uc0b0\ub354 \ud5e4\uc774\uadf8\ub294 \ub808\uc774\uac74\uc758 \uc870\uc5b8\uc790\ub4e4\uc744 \ubb34\uc5c7\uc774\ub77c\uace0 \ubb18\uc0ac\ud558\uc600\ub294\uac00?"
 
 print (a)
-"""
-
-f = open ()
