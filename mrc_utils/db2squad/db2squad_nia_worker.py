f22041d2454b33b5778795fe04f8f7717c40ef0d
yeongsunpark <ylunar@naver.com>
2019-05-07 17:43:38 +0900

    worker 사용 #4

--- mrc_utils/db2squad/db2squad_nia_worker.py
+++ mrc_utils/db2squad/db2squad_nia_worker.py
@@ -16,7 +16,7 @@ import pymysql
 
 sys.path.append(os.path.abspath('..'))
 import custom_logger
-from morp_analyze_my import NLPAnalyzer
+# from morp_analyze_my import NLPAnalyzer
 
 
 def return_myself(token):
@@ -32,13 +32,14 @@ class SquadDb():
     def __init__(self):
         ###user_input#########################################################
         self.db_table = "SQUAD_NEWS_NIA"
-        self.data_output_dir = "/home/msl/ys/cute/nia/check"
-        self.version = "0508"
+        self.data_output_dir = "/home/msl/ys/squad"
+        self.lang = "no"
+        self.version = "0506"
         self.test_ratio = 0.2    # dev_ratio (8:1:1로 나누기 위해)
+        self.maximum = None
+        self.is_divide = False
         self.is_dp = False
         self.is_random = True
-        self.mode = "normal"
-        self.split = True
         #######################################################################
 
 
@@ -49,6 +50,8 @@ class SquadDb():
         self.cur = None
         self.random_end = "ORDER BY RAND()" if self.is_random else ""
         self.dp_end = "_dp" if self.is_dp else ""
+        self.context_ori = ""
+        self.processed_ctx_list = list()
 
     def easy_mysql(self, cfg_dict, encoding='utf8', autocommit=False):
         self.con = pymysql.connect(host=cfg_dict['host'], user=cfg_dict['usr'],
@@ -61,7 +64,7 @@ class SquadDb():
         try:        # try to connect to project db
             #cfg_dict = dict(host=self.db_cnf_dict['host'], usr=self.db_cnf_dict['usr'],
             #                pwd=self.db_cnf_dict['pwd'], db=self.db_cnf_dict['db'])
-            cfg_dict = dict(host='localhost', usr= 'root', pwd='data~secret!', db=table_name)
+            cfg_dict = dict(host='10.122.64.83', usr= 'root', pwd='data~secret!', db=table_name)
             #self.cfg_dict = cfg_dict
             #self.easy_mysql(cfg_dict, encoding=self.db_cnf_dict['encoding'], autocommit=True)     # turn-on autocummit, be careful!
             self.easy_mysql(cfg_dict, encoding='utf8', autocommit=True)
@@ -72,82 +75,98 @@ class SquadDb():
 
     def db2squad(self):
         # fetch_sql_ctx = "SELECT id, title, context, context_morph, context_dp FROM all_context_all {};".format(self.random_end)
-        fetch_sql_ctx = "SELECT id, title, context, source FROM all_context_all where season=1 or season=2 or season=3 or season=6 {};".format(self.random_end)
+        fetch_sql_ctx = "SELECT id, title, context, source FROM all_context_all where season in (1, 2, 3, 6) {};".format(self.random_end)
         # fetch_sql_ctx = "SELECT CTX.id, CTX.title, CTX.context  FROM all_context as CTX INNER JOIN all_qna as QA on QA.c_id = CTX.id WHERE CTX.season = 5 and QA.question IS NOT NULL AND QA.ANSWER IS NOT NULL GROUP BY CTX.id {} ".format(self.random_end)
         self.cur.execute(fetch_sql_ctx)
-        contexts = self.cur.fetchall()   # entire
-
+        contextss = self.cur.fetchall()   # entire
+        print (len(contextss))
+        """
         contexts_not_fix = [c for c in contexts]  # not fix
         # contexts_not_fix = [c for c in contexts if c[0] not in contexts_dev_fix_ids]  # not fix
         logger.info("len(contexts): {}".format(len(contexts)))
-        # logger.info("len(contexts_dev_fix_ids): {}".format(len(contexts_dev_fix_ids))) # 없지. 
+        # logger.info("len(contexts_dev_fix_ids): {}".format(len(contexts_dev_fix_ids))) # 없지.
         logger.info("len(contexts_not_fix): {}".format(len(contexts_not_fix)))  # 위에 것과 같아야함.
 
-        if self.split :
-            train_cnt = math.floor((len(contexts)) * (1 - self.test_ratio))
-            context_train = random.sample(contexts_not_fix, train_cnt)  # 0.8은 여기
-            c_dev_tmp1 = [c for c in contexts_not_fix if c not in context_train]  # 나머지는 여기
-            # c_dev_tmp2 = list(contexts_dev_fix)  # 없음.
-            logger.info("c_dev_tmp1: {}".format(len(c_dev_tmp1)))
-            # logger.info("c_dev_tmp2: {}".format(len(c_dev_tmp2)))
-            context_dNt = c_dev_tmp1  # 0.2는 여기 (그냥 한 번 더 넣을래)
-            # context_dNt = c_dev_tmp1 + c_dev_tmp2  # 0.2는 여기
-            dNt_cnt = math.floor((len(contexts) - len(context_train)) * 0.5)  # 전체 본문 길이 - train 본문(80%) 길이 / 2 -> 10%
-            context_dev = random.sample(context_dNt, dNt_cnt)
-            context_test = [c for c in contexts if c not in context_train and c not in context_dev]
-            logger.info("train_cnt: {}".format(train_cnt))
-            logger.info("Len context_train: {}".format(len(context_train)))
-            logger.info("Len context_dev: {}".format(len(context_dev)))
-            logger.info("Len context_test: {}".format(len(context_test)))
-        else:
-            context_all = contexts
+        train_cnt = math.floor((len(contexts))*(1-self.test_ratio))
+        context_train = random.sample(contexts_not_fix, train_cnt)  # 0.8은 여기
+        c_dev_tmp1 = [c for c in contexts_not_fix if c not in context_train]  # 나머지는 여기
+        # c_dev_tmp2 = list(contexts_dev_fix)  # 없음.
+        logger.info("c_dev_tmp1: {}".format(len(c_dev_tmp1)))
+        # logger.info("c_dev_tmp2: {}".format(len(c_dev_tmp2)))
+        context_dNt = c_dev_tmp1  # 0.2는 여기 (그냥 한 번 더 넣을래)
+        # context_dNt = c_dev_tmp1 + c_dev_tmp2  # 0.2는 여기
+        dNt_cnt = math.floor((len(contexts) - len(context_train)) * 0.5)  # 전체 본문 길이 - train 본문(80%) 길이 / 2 -> 10%
+        context_dev = random.sample(context_dNt, dNt_cnt)
+        context_test = [c for c in contexts if c not in context_train and c not in context_dev]
+        logger.info("train_cnt: {}".format(train_cnt))
+        logger.info("Len context_train: {}".format(len(context_train)))
+        logger.info("Len context_dev: {}".format(len(context_dev)))
+        logger.info("Len context_test: {}".format(len(context_test)))
+        """
+        workers = 30
+        r = 1000
+        # cqas(contextss, 0, 2)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as exe:
+            fs = {exe.submit(cqas, contextss, n, r) for n in range(0, len(contextss), r)}
 
-        for data_type in ["train", "dev", "test"]:
-        # for data_type in ["all"]:
-            cnt = 0
-            result = dict()
-            result['version'] = self.version
-            result['creator'] = "MINDs Lab."
-            result['data'] = list()
-            logger.info("Data type {} start..".format(data_type))
-            for context in eval("context_{}".format(data_type)):
-                data_dict = dict()      # each context
-                data_dict['title'] = context[1]
-                data_dict['source'] = context[3]
-                data_dict['paragraphs'] = list()
-                para_dict = dict()
-                try:
-                    para_dict['context'] = context[2]
-                except IndexError:
-                    logger.critical(context)
-                    exit()
+def cqas(contexts, n, r):
+        con = None
+        cur = None
+        cfg_dict = dict(host='10.122.64.83', usr='root', pwd='data~secret!', db='SQUAD_NEWS_NIA')
+        con = pymysql.connect(host=cfg_dict['host'], user=cfg_dict['usr'],
+                                   passwd=cfg_dict['pwd'], db=cfg_dict['db'], charset='utf8')
+        cur = con.cursor()
+        # exec("j{} = SquadDb()".format(n))
+        cur.execute("SET NAMES utf8")
+        logger.info("processing: {} ..< {}".format(n, n + r))
 
-                qas_list = list()
-                fetch_sql_qa = "SELECT q_id, question, answer_start, answer, classType FROM all_qna " \
-                                    "WHERE c_id='{}' and q_id not like '%-2'".format(context[0])
-                self.cur.execute(fetch_sql_qa)
-                for row in self.cur.fetchall():
-                    qa = {'id': row[0], 'answers': [{'answer_start': row[2], 'text': row[3]}],
-                                'question': row[1], 'classtype':row[4]}
-                    qas_list.append(qa)
-                    cnt += 1
-                para_dict['qas'] = qas_list
-                if qas_list == "":
-                    continue
-                else:
-                    data_dict['paragraphs'].append(para_dict)
-                    result['data'].append(data_dict)
-            logger.info("Finish creating json structure({})".format(data_type))
+        cnt = 0
+        result = dict()
+        result['version'] = 1
+        result['creator'] = "MINDs Lab."
+        result['data'] = list()
 
-            with open(os.path.join(self.data_output_dir, "ko_nia_v{}_squad{}_{}.json".
-                    format(self.version, self.dp_end, data_type)),
-                      'w', encoding='utf8') as fp:
-                json.dump(result, fp, ensure_ascii=False)
-            with open(os.path.join(self.data_output_dir, "ko_nia_v{}_squad_pretty{}_{}.json".
-                    format(self.version, self.dp_end, data_type)),
-                      'w', encoding='utf8') as fp:
-                json.dump(result, fp, ensure_ascii=False, indent=2)
-            logger.info("Data dump {} finished..: len {}".format(data_type, cnt))
+        for context in contexts[n:n + r]:
+            # print (len(context))
+
+            data_dict = dict()  # each context
+            data_dict['title'] = context[1]
+            data_dict['source'] = context[3]
+            data_dict['paragraphs'] = list()
+            para_dict = dict()
+            try:
+                para_dict['context'] = context[2]
+            except IndexError:
+                logger.critical(context)
+                exit()
+
+            qas_list = list()
+            fetch_sql_qa = "SELECT q_id, question, answer_start, answer, substring_index(classType,'_',2) FROM all_qna " \
+                           "WHERE c_id='{}' and q_id like '%-1'".format(context[0])
+            cur.execute(fetch_sql_qa)
+
+            for row in cur.fetchall():
+                qa = {'id': row[0], 'answers': [{'answer_start': row[2], 'text': row[3]}],
+                      'question': row[1], 'classtype': row[4]}
+                qas_list.append(qa)
+                cnt += 1
+            para_dict['qas'] = qas_list
+            if qas_list == "":
+                continue
+            else:
+                data_dict['paragraphs'].append(para_dict)
+                result['data'].append(data_dict)
+        logger.info("Finish creating json structure({})".format(n))
+
+        with open(os.path.join("/home/msl/ys/squad", "ko_nia_v{}_squad{}_{}.json".
+                format("0", "0", n)),
+                  'w', encoding='utf8') as fp:
+            json.dump(result, fp, ensure_ascii=False)
+        with open(os.path.join("/home/msl/ys/squad", "ko_nia_v{}_squad_pretty{}_{}.json".
+                format("0", "0", n)),
+                  'w', encoding='utf8') as fp:
+            json.dump(result, fp, ensure_ascii=False, indent=2)
+        logger.info("Data dump {} finished..: len {}".format(n, cnt))
 
 
 if __name__ == "__main__":
