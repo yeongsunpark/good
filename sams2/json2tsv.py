8346c2d859f0081d5301155acc7d82c2e76d2501
yeongsunpark <ylunar@naver.com>
2019-08-01 14:33:38 +0900

    #38 경로만 바꿔줌

--- sams2/json2tsv.py
+++ sams2/json2tsv.py
@@ -6,13 +6,10 @@
 import json
 
 class abc():
-    def __init__(self):
-        self.read_json = "/home/msl/ys/cute/data/sams2/entity_200_all_v2.json"
-        self.write_json = "/home/msl/ys/cute/data/sams2/entity_200_all_v2.txt"
-    def main(self):
-        with open(self.read_json, "r", encoding='utf-8') as f1:
+    def main(self, r_json, w_json):
+        with open(r_json, "r", encoding='utf-8') as f1:
             json_data1 = json.load(f1)
-        f2 = open(self.write_json, "w")
+        f2 = open(w_json, "w")
 
         for doc in json_data1['data']:
             for p in doc['paragraphs']:
@@ -28,7 +25,10 @@ class abc():
                         f2.write("\t".join(item))
                         f2.write("\n")
         f2.close()
+        return 1
 
 if __name__ == "__main__":
     a = abc()
-    b = a.main()
+    read_json = "/home/msl/ys/cute/data/sams2/random_questions_from_m4_5_to_6.json"
+    write_json = "/home/msl/ys/cute/data/sams2/random_questions_from_m4_5_to_6.txt"
+    b = a.main(read_json, write_json)
