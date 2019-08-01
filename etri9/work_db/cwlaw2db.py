2803569023e6a69e0d22ebea43dcd3b48691e67e
yeongsunpark <ylunar@naver.com>
2019-08-01 11:44:39 +0900

    issue #39 column 추가된 db 에 맞춰 코드 수정하였음.

--- etri9/work_db/cwlaw2db.py
+++ etri9/work_db/cwlaw2db.py
@@ -11,20 +11,22 @@ sys.path.append(os.path.abspath('..'))
 class SquadDb(SquadDbSuper):
     def __init__(self):
         super(SquadDb, self).__init__()  # pull_module.SquadDbSuper 의 __init__ (self) 아래 속성 가져옴.
-        self.input_dir = "/home/msl/ys/cute/data/re_law/파일럿과1차"
 
     def connect_db2(self):
         cfg_dict = self.connect_db()  # cfg_dict = pull_module.SquadDb.connect_db(self)
         self.cur = self.easy_mysql(cfg_dict)  # self.cur = pull_module.SquadDb.easy_mysql(self, cfg_dict)
 
 
-    def insert_context(self):
+    def insert_context(self, max_c_id, season, input_dir):
         print('insert start')
-        max_c_id = 1
-        for fl in os.listdir(self.input_dir):
-            with open(os.path.join(self.input_dir, fl), "r") as f:
+        for fl in os.listdir(input_dir):
+            with open(os.path.join(input_dir, fl), "r") as f:
+                print (fl)
                 json_data1 = json.load(f)
-                name = fl.split("_")[1]
+                if fl.count("_") == 2:
+                    name = fl.split("_")[1]
+                else:
+                    name = fl.split("_")[0]
                 json_data = json_data1[name]
 
                 document_id = json_data["문서ID"]
@@ -38,25 +40,29 @@ class SquadDb(SquadDbSuper):
                 article_content = json_data["조내용"]
                 law_in_q = json_data["메모"]
                 file_name = json_data["파일명"].split("_")[0]
+                ref_context = json_data["질문참고본문"]
                 try:
-                    insert_memo_sql = 'INSERT INTO all_context values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+                    insert_memo_sql = 'INSERT INTO all_context values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
                     category_id = max_c_id
 
                     self.cur.execute(insert_memo_sql, (category_id, document_id, statute_code, law_name, public_date,
                                                        belong_no, belong_title, article_no, article_title, article_content,
-                                                       law_in_q, file_name))
+                                                       law_in_q, ref_context, file_name, season))
                     self.con.commit()
                     max_c_id += 1
                 except:
                     print("try again")
 
-    def insert_question(self, max_q_id):
+    def insert_question(self, max_q_id, input_dir):
         print('insert start')
-        for fl in os.listdir(self.input_dir):
-            with open(os.path.join(self.input_dir, fl), "r") as f:
+        for fl in os.listdir(input_dir):
+            with open(os.path.join(input_dir, fl), "r") as f:
                 print (fl)
                 json_data1 = json.load(f)
-                name = fl.split("_")[1]
+                if fl.count("_") == 2:
+                    name = fl.split("_")[1]
+                else:
+                    name = fl.split("_")[0]
                 json_data = json_data1[name]
                 document_id = json_data["문서ID"]
                 for i in range(len(json_data["qas"])):
@@ -70,16 +76,17 @@ class SquadDb(SquadDbSuper):
                     text = answers["text"]
 
                     c_id = j.select_c_id(document_id)
-
                     try:
-                        insert_memo_sql = 'INSERT INTO all_qna values(%s, %s, %s, %s, %s, %s, %s)'
+                        insert_memo_sql = 'INSERT INTO all_qna values(%s, %s, %s, %s, %s, %s, %s, %s)'
                         q_id = max_q_id
                         self.cur.execute(insert_memo_sql,
-                                         (c_id, q_id, question, answer_start, answer_end, text, level))
+                                         (c_id, document_id, q_id, question, answer_start, answer_end, text, level))
                         self.con.commit()
                         max_q_id += 1
                     except:
                         print("try again")
+                        print (c_id, q_id, document_id, question, answer_start, answer_end, text, level)
+
 
     def select_c_id(self, document_id):
         select_sql = 'select id from all_context where document_id = %s'
@@ -89,19 +96,42 @@ class SquadDb(SquadDbSuper):
         if len(selected_c_id) == 1:
             return selected_c_id[0][0]
 
-    def max_q(self):
-        select_max_q = 'select max(abs(q_id)) from all_qna'
-        self.cur.execute(select_max_q)
-        selected_max_q = self.cur.fetchall()
+    def max_id(self, type):
+        if type == "question":
+            select_max = 'select max(abs(q_id)) from all_qna'
+        elif type == "context":
+            select_max = 'select max(abs(id)) from all_context'
+        self.cur.execute(select_max)
+        selected_max = self.cur.fetchall()
         self.con.commit()
-        print (len(selected_max_q))
-        if len(selected_max_q) == 1:
+        print (len(selected_max))
+        if selected_max[0][0] is None:
             return 0
         else:
-            return int(selected_max_q[0][0])
+            return int(selected_max[0][0])
 
 if __name__ == "__main__":
+    # context 후 question 하기!
+    mode = "question"  # context/ question
+    season = 2
+    if season == 0:
+        input_dir = "/home/msl/ys/cute/data/re_law/파일럿과1차_190731/답과3가지질문만들기-파일럿(374건)/답과3가지질문만들기-파일럿(374건)"
+    elif season == 1:
+        input_dir = "/home/msl/ys/cute/data/re_law/파일럿과1차_190731/답과3가지질문만들기-1차(1474건)/답과3가지질문만들기-1차(1474건)"
+    elif season == 2:
+        input_dir = "/home/msl/ys/cute/data/re_law/2219-답과3가지질문만들기-2차"
+    else:
+        print ("no season")
+
     j = SquadDb()
     j.connect_db2()
-    m_q_id = j.max_q()
-    j.insert_question(m_q_id+1)
+
+    print (j.max_id("question"))
+
+    if mode == "context":
+        m_c_id = j.max_id(mode) + 1
+        j.insert_context(m_c_id, season, input_dir)
+    if mode == "question":
+        m_q_id = j.max_id(mode) + 1
+        print(m_q_id)
+        j.insert_question(m_q_id, input_dir)
