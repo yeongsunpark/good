569126d2bb4a0ce59ece328e00fba3448641124a
yeongsunpark <ylunar@naver.com>
2019-08-06 13:46:50 +0900

    #44 log관련

---
+++ etri9/work_db/ys_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created by YeongsunPark at 2019-07-24
+
+import logging
+# custom_logger.py 와 구분을 위해 이름 바꿈
+
+class MyHandler(logging.StreamHandler):
+
+    def __init__(self):
+        logging.StreamHandler.__init__(self)
+        fmt = '[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s >>> %(message)s'
+        fmt_date = '%Y-%m-%d_%T %Z'
+        formatter = logging.Formatter(fmt, fmt_date)
+        self.setFormatter(formatter)
