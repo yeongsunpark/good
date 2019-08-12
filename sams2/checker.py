44ea115ad4894c334e9309c76ec8dc647f9404c8
yeongsunpark <ylunar@naver.com>
2019-08-12 15:03:55 +0900

    #47 본문과 비교하는 기능

--- sams2/checker.py
+++ sams2/checker.py
@@ -9,6 +9,8 @@ sys.path.insert(0,'..')
 import ys_logger
 from marker_checker import marker_checker
 from three_checker import three_checker
+from comp_with_ori import comp_with_ori
+
 # log
 logger = logging.getLogger('root')
 logger.setLevel("DEBUG") # INFO
@@ -22,7 +24,7 @@ class check():
 
     def main(self):
         for f in os.listdir(self.input_dir):
-            if "squad" in f:
+            if "2" in f:
                 continue
             header = True
             logger.info("File {} start..".format(f))
@@ -45,9 +47,11 @@ class check():
                         mod_context = item[7]
                         remarks = item[8]
                     else:
+                        logger.error("Check len item")
                         break
-                    marker_checker(ind, context)  # 마커 개수 확인
-                    three_checker(ind, classify, mod_question, mod_answer, mod_context)  # 분류와 체커 확인
+                    # marker_checker(ind, context)  # 마커 개수 확인
+                    # three_checker(ind, classify, mod_question, mod_answer, mod_context)  # 분류와 체커 확인
+                    comp_with_ori(ind, q_id, context, question, answer, mod_question, mod_answer, mod_context, remarks)  # 원본과 비교
 
 
 if __name__ == '__main__':
